d4a0b060c9c6bfcb953a5b1bb8e1435692ae5c57
Sam Salfi <samsalfi@yahoo.ca>
2023-12-16 21:49:59 -0400

    Cleanup
    Added docker-compose for Postgres database
    Removed useless folders and java files
    Added lib folder
    Added .vscode folder with project config

--- src/graph/antlr4test.java
+++ src/graph/antlr4test.java
@@ -1,17 +1,20 @@
 package graph;
+
+import db.DBHelper;
 import org.antlr.v4.runtime.CharStreams;
 
 import java.util.Scanner;
 
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.*;
 import graph.grammar.*;
-public class antlr4test {
+
+public class Antlr4test {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         System.out.println("Enter a query: ");
-        String inp = "CREATE (person:Person {name: \"Sam Salfi\", age: twentytwo})\n" + "MATCH (person:Person)-[likes:LIKES]-(movie:Movie)\n" +
+        String inp = "CREATE (person:Person {name: \"Sam Salfi\", age: twentytwo})\n"
+                + "MATCH (person:Person)-[likes:LIKES]-(movie:Movie)\n" +
                 "RETURN person.name, person.born\n";
 
         // String input = scn.nextLine();
@@ -22,7 +25,9 @@ public class antlr4test {
         var visitor = new CypherVisitor();
         var results = visitor.visit(parser.query());
         System.out.println(results);
-    }
 
+        DBHelper dbHelper = new DBHelper("buzzhub_nodes", "buzzhub_relationships");
+
+    }
 
 }
